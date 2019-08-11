3b81d91e73fb98a241b26559f9f3e9ef9172b1c4
mdarocha <11572618+mdarocha@users.noreply.github.com>
2019-08-11 11:34:00 +0200

    Add substitue for ~

--- window-title.plugin.zsh
+++ window-title.plugin.zsh
@@ -1,5 +1,5 @@
 case $TERM in
     xterm*|termite)
-	precmd () {print -Pn "\e]0;[${PWD}] {$(history | tail -n1 | awk '{for (i=2;i<=NF-1;i++) printf $i " "; print $NF}')}\a"}
+	precmd () {print -Pn "\e]0;${PWD/#$HOME/~} [$(history | tail -n1 | awk '{for (i=2;i<=NF-1;i++) printf $i " "; print $NF}')]\a"}
         ;;
 esac
